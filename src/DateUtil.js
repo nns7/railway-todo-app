a88798f977f8e3fda455d84221a163b3da6dd49d
nns7 <nns7snn@gmail.com>
2022-11-29 21:55:32 +0900

    :+1: 期限日時を過ぎていたら期限切れと表示するよう修正。

--- src/DateUtil.js
+++ src/DateUtil.js
@@ -1,14 +1,17 @@
 import { format, formatDistanceStrict, parse } from "date-fns";
 import { ja } from "date-fns/locale";
 
-export function showJST(utc) {
-  const timeUtc = parse(utc, "yyyy-MM-dd'T'HH:mm:ss'Z'", new Date());
-  timeUtc.setHours(timeUtc.getHours() + 9);
-  return format(timeUtc, "yyyy年MM月dd日 HH時mm分");
+export function showJST(limit) {
+  const limitDate = parse(limit, "yyyy-MM-dd'T'HH:mm:ss'Z'", new Date());
+  limitDate.setHours(limitDate.getHours() + 9);
+  return format(limitDate, "yyyy年MM月dd日 HH時mm分");
 }
 
-export function timeRemainingFromNow(utc) {
-  const timeUtc = parse(utc, "yyyy-MM-dd'T'HH:mm:ss'Z'", new Date());
-  timeUtc.setHours(timeUtc.getHours() + 9);
-  return formatDistanceStrict(timeUtc, new Date(), { locale: ja });
+export function timeRemainingFromNow(limit) {
+  const limitDate = parse(limit, "yyyy-MM-dd'T'HH:mm:ss'Z'", new Date());
+  limitDate.setHours(limitDate.getHours() + 9);
+  const nowDate = new Date();
+  return limitDate > nowDate
+    ? formatDistanceStrict(limitDate, nowDate, { locale: ja })
+    : "期限切れ";
 }
